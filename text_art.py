df0845e4cd1e0a1bf11f9217bfdcfe1fb1dfb73b
muchi21 <waylenmu@gmail.com>
2024-02-25 02:27:41 +0800

    upload coding

---
+++ text_art.py
@@ -0,0 +1,46 @@
+import os
+import numpy as np
+from PIL import Image
+import requests
+
+
+WIDTH = 200
+image_path = 'unnamed.jpg'
+file_output_file = 'text_art.txt'
+
+
+# Open the image file
+img = Image.open(image_path).convert('L')  # convert image to 8-bit grayscale
+
+# Define the ASCII characters that will represent the grayscale values
+# The list of characters is sorted by increasing "density"
+ASCII_CHARS = ["@", "#", "S", "%", "?", "*", "+", ";", ":", ",", "."]
+
+# Resize the image according to a new width
+
+width, height = img.size
+ratio = height / width
+new_height = int(WIDTH * ratio)
+img = img.resize((WIDTH, new_height))
+
+# Convert each pixel to grayscale
+pixels = np.array(img)
+
+# Normalize the pixels between 0 and 1 and multiply by the number of ASCII characters
+normalized_pixels = pixels / 255
+ascii_pixels = (normalized_pixels * (len(ASCII_CHARS) - 1)).astype(int)
+
+# Map each normalized pixel to an ASCII character
+ascii_image = [" ".join([ASCII_CHARS[pixel] for pixel in row]) for row in ascii_pixels]
+
+ascii_image_text = "\n".join(ascii_image)
+
+# Open the code to see the text art in terminal
+# print(ascii_image_text)
+
+with open(file_output_file, 'w') as file:
+    file.write(ascii_image_text)
+
+os.system(f'notepad {file_output_file}')
+
+print("[info] complete")

7d1b735b66fd179085a74fd29113531d45575e68
MuChi2112 <137669541+MuChi2112@users.noreply.github.com>
2024-02-28 10:10:58 +0800

    Update discord_text_art.py

--- discord_text_art.py
+++ discord_text_art.py
@@ -17,9 +17,10 @@ intents.messages = True
 intents.message_content = True
 
 client = discord.Client(intents=intents)
-# ASCII_CHARS = ["@", "#", "S", "%", "?", "*", "+", ";", ":", ",", "."]
 
-ASCII_CHARS = ["█", "▓", "@","G","8","L", "*", "+", ";", ":", ",", " "]
+ASCII_CHARS = ["@", "#", "S", "%", "?", "*", "+", ";", ":", ",", "."]
+
+# ASCII_CHARS = ["█", "▓", "@","G","8","L", "*", "+", ";", ":", ",", " "]
 
 @client.event
 async def on_ready():
